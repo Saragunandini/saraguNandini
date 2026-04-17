ad9f04b9471f794bb54ceccbcfef81c33960038c
Saragu Nandini <nandini.saragu@gmail.com>
2026-04-17 19:36:40 +0100

    Update LoginSteps.cs

--- Tests/StepDefinitions/LoginSteps.cs
+++ Tests/StepDefinitions/LoginSteps.cs
@@ -1,1 +1,35 @@
+using TechTalk.SpecFlow;
+using Tests.Drivers;
+
+namespace Tests.StepDefinitions;
+
+[Binding]
+public class LoginSteps
+{
+    private readonly PlaywrightDriver _driver;
+
+    public LoginSteps(PlaywrightDriver driver)
+    {
+        _driver = driver;
+    }
+
+    [Given(@"I open login page")]
+    public async Task GivenIOpenLoginPage()
+    {
+        await _driver.Page.GotoAsync("https://example.com/login");
+    }
+
+    [When(@"I enter credentials")]
+    public async Task WhenIEnterCredentials()
+    {
+        await _driver.Page.FillAsync("#username", "test");
+        await _driver.Page.FillAsync("#password", "test123");
+    }
+
+    [Then(@"I should see dashboard")]
+    public async Task ThenIShouldSeeDashboard()
+    {
+        await _driver.Page.WaitForURLAsync("**/dashboard");
+    }
+}
 
