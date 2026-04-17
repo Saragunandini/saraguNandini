8cde6fde6960bfb1cf912783ec308b5eec381b91
Saragu Nandini <nandini.saragu@gmail.com>
2026-04-17 19:38:41 +0100

    Update TestHooks.cs

--- Tests/Hooks/TestHooks.cs
+++ Tests/Hooks/TestHooks.cs
@@ -1,1 +1,28 @@
+using TechTalk.SpecFlow;
+using Tests.Drivers;
+
+namespace Tests.Hooks;
+
+[Binding]
+public class TestHooks
+{
+    private readonly PlaywrightDriver _driver;
+
+    public TestHooks(PlaywrightDriver driver)
+    {
+        _driver = driver;
+    }
+
+    [BeforeScenario]
+    public async Task BeforeScenario()
+    {
+        await _driver.Init();
+    }
+
+    [AfterScenario]
+    public async Task AfterScenario()
+    {
+        await _driver.Dispose();
+    }
+}
 
