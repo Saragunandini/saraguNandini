d70e1ae54e4fee00cfe161498657acabcfe4610e
Saragu Nandini <nandini.saragu@gmail.com>
2026-04-17 19:38:09 +0100

    Update PlaywrightDriver.cs

--- Tests/Drivers/PlaywrightDriver.cs
+++ Tests/Drivers/PlaywrightDriver.cs
@@ -1,1 +1,29 @@
+using Microsoft.Playwright;
 
+namespace Tests.Drivers;
+
+public class PlaywrightDriver
+{
+    public IPlaywright Playwright;
+    public IBrowser Browser;
+    public IBrowserContext Context;
+    public IPage Page;
+
+    public async Task Init()
+    {
+        Playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+
+        Browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = true
+        });
+
+        Context = await Browser.NewContextAsync();
+        Page = await Context.NewPageAsync();
+    }
+
+    public async Task Dispose()
+    {
+        await Browser.CloseAsync();
+    }
+}
